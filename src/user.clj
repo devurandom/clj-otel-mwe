469d9e5f35c72571eb2a28dacfe228214858cd31
Dennis Schridde <heri@mailbox.org>
2024-05-18 17:18:44 +0200

    Minimize example further

--- src/user.clj
+++ src/user.clj
@@ -2,20 +2,17 @@
  (:require
   [clojure.string :as str]
   [ring.adapter.jetty :as jetty]
-  [steffan-westcott.clj-otel.api.trace.span :as span]
   [steffan-westcott.clj-otel.context :as context]
   [taoensso.timbre :as log]))
 
 (defn- handler
  [_]
- (println "OUT SPAN" (context/current))
- (span/with-span! {:name "TEST"}
-   (println "IN SPAN" (context/current))))
+ (println "CONTEXT" (context/current)))
 
 (defn- otel-context-middleware
  [{:keys [?ns-str] :as data}]
  (when-not (str/starts-with? ?ns-str "io.opentelemetry.javaagent.")
-   (span/get-span-context))
+   (context/current))
  data)
 
 (defn -main
